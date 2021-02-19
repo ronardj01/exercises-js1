07b398219ad8b61ddaf37858b6b9dd983a8571a5
ronardj01 <ronardj01@gmail.com>
2021-02-19 18:52:24 +0100

    Completed K-functions exercises

--- week-1/Homework/K-functions-parameters/exercise5.js
+++ week-1/Homework/K-functions-parameters/exercise5.js
@@ -1,4 +1,7 @@
 // Declare your function here
+function createLongGreeting(nombre, edad) {
+    return `Hello, my name is ${nombre} and I'm ${edad} old`
+}
 
 const greeting = createLongGreeting("Daniel", 30);
 
