52685e9ebf96423cd95a3ba5dd0b19ddf601e6a1
ronardj01 <ronardj01@gmail.com>
2021-02-19 20:52:36 +0100

    Completed L-functions exercises

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,42 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+// Ejercicio 1.
+
+function percentage(amt, total) {
+    let porciento = amt / total * 100;
+    let result = Math.round(porciento);
+    return result;
+}
+
+function createGreeting(stdOrMentor,amt,total) {
+    let porciento = percentage(amt, total);
+    const mensaje = `Percentage ${stdOrMentor}: ${porciento}%`;
+    return mensaje;
+}
+
+let estudiantes = createGreeting("students", 23 ,37);
+let profesores = createGreeting("mentors",14 ,37);
+
+console.log(estudiantes);
+console.log(profesores);
+console.log("");
+
+// Ejercicio 2.
+console.log("*****///////////////////////*****");
+
+function changeUppercase (toChange) {
+    return toChange.toUpperCase();
+}
+
+function uppercaseGreeting(saludo, nombre) {
+    let mensaje = `${changeUppercase(saludo)} ${changeUppercase(nombre)}`;
+    return mensaje;
+}
+
+let saludo1 = uppercaseGreeting("hello", mentor1);
+let saludo2 = uppercaseGreeting("hola", mentor2);
+
+console.log(saludo1);
+console.log(saludo2);
