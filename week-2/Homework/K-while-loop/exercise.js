6305542c27170719f981a694e16de3a04ae7b83c
ronardj01 <ronardj01@gmail.com>
2021-03-02 11:45:54 -0400

    Completed K-while exercise

--- week-2/Homework/K-while-loop/exercise.js
+++ week-2/Homework/K-while-loop/exercise.js
@@ -8,8 +8,15 @@
 
 let n = 10;
 
-function sumTillNum(num){
-	//your code here
+function sumTillNum(num) {
+  //your code here
+  let sum = 0;
+  while (num > 0) {
+    sum += num;
+    num--;
+  }
+  return sum;
 }
 
 console.log("Sum from 0 to " + n + " is: " + sumTillNum(n));
+
