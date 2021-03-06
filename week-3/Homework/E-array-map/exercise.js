8c7354fd0125d1fe2cb63b716525a4566825b0b3
ronardj01 <ronardj01@gmail.com>
2021-03-06 06:39:01 -0400

    Completed E-array-map exercise

--- week-3/Homework/E-array-map/exercise.js
+++ week-3/Homework/E-array-map/exercise.js
@@ -1,5 +1,24 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
+//Callback funcion idependiente
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+function multimplicar(number) {
+    return number * 100;
+}
+
+let numberProducto = numbers.map(multimplicar);
+console.log(numberProducto);
+
+//Callback funcion interna
+let numberProducto1 = numbers.map(function multiplicar1(number) { return number * 100 })
+console.log(numberProducto1);
+
+//Callback funcion anonima
+let numberProducto2 = numbers.map(function (number) { return number * 100 })
+console.log(numberProducto2)
+
+//Callback funcion flecha
+let numberProducto3 = numbers.map(number => number * 100)
+console.log(numberProducto3)
